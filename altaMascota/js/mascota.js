b2cc4ea8d270a74aaa726b9379f39054f543f5d7
upszot <upszot@gmail.com>
2019-09-17 19:01:18 -0300

    Clase 5

--- altaMascota/js/mascota.js
+++ altaMascota/js/mascota.js
@@ -1,4 +1,17 @@
 function Mascota(nombre, edad, tipo, castrado, vacunado, desparasitado, alimento)
 {
-    
+    this.nombre=nombre;
+    this.edad=edad;
+    this.tipo=tipo;
+    this.castrado=castrado;
+    this.vacunado=vacunado;
+    this.desparasitado=desparasitado;
+    this.alimento=alimento;
+
+    Mascota.prototype.toString=function(){
+        
+        return `Hola soy $(this.nombre) y tengo $(this.edad)`;
+    }
 }
+
+let m1=new Mascota("bobby",3,"perro",true,false,true,"carne");
