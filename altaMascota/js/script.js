1c8f5a27c35b58a5469110c4b4687aaae5886e76
upszot <upszot@gmail.com>
2019-09-17 19:42:01 -0300

    Clase 5... me duermoooooooooooooo

--- altaMascota/js/script.js
+++ altaMascota/js/script.js
@@ -1,6 +1,20 @@
+var frm;
+
 window.addEventListener("load", inicializarManejadores);
 
-function inicializarManejadores()
-{
+function inicializarManejadores() {
+    frm = document.forms[0];
+        
+    //callback = le paso una tarea a alguien que en algun momento lo va a ejecutar
+    frm.addEventListener('submit', manejadorSubmit);
     
+    //es lo mismo que lo de arriba
+    // frm.onsubmit=function(){};
+
+    
+}
+
+function manejadorSubmit(e) 
+{
+    e.preventDefault();
 }
