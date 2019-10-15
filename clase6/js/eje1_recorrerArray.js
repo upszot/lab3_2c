17a008678bfd6314f73b6b51ceba374f332fde7f
alumno <alumno@utnfra.com.ar>
2019-10-15 19:13:00 -0300

    clase 6

---
+++ clase6/js/eje1_recorrerArray.js
@@ -0,0 +1,31 @@
+//window.addEventListener('load', ejem1);
+window.addEventListener('load', ejem3);
+
+function ejem1()
+{
+    let array=["juan","ana",true,23,[1,2,3],{nombre:"jorge,edad:30"}];
+    console.log("Ejemplo 1");
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i]);
+        
+    }
+}
+
+function ejem2()
+{
+    let array=["juan","ana",true,23,[1,2,3],{nombre:"jorge,edad:30"}];
+    console.log("Ejemplo 2");
+    for (const elemento of array) {
+        console.log(elemento);
+    }
+
+}
+
+function ejem3()
+{
+    let array=["juan","ana",true,23,[1,2,3],{nombre:"jorge,edad:30"}];
+    console.log("Ejemplo 3");
+    array.forEach( elemento =>  console.log(elemento) );
+    
+
+}
