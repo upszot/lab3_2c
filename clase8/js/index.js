dc2418962630f1eff43b1a942873ceb33d2c3dbf
upszot <upszot@gmail.com>
2019-10-29 19:13:46 -0300

    clase 8

---
+++ clase8/js/index.js
@@ -0,0 +1,11 @@
+
+/*
+lo que logra que funcione con el defer en el scr del index
+let selPaises = document.getElementById("selPaises");
+console.log(selPaises);
+*/
+
+window.addEventListener('load',function(){
+    selPaises = document.getElementById("selPaises");
+    selCiudades = document.getElementById("selCiudades");
+});
