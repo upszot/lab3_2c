4f96e58a16176058352cdfff0be21e18942ef8de
upszot <upszot@gmail.com>
2019-10-29 22:10:13 -0300

    clase 8 - bootstrap

--- clase8/js/index.js
+++ clase8/js/index.js
@@ -1,86 +1,61 @@
+let selPaises;
+let selCiudades;
 
-/*
-lo que logra que funcione con el defer en el scr del index
-let selPaises = document.getElementById("selPaises");
-console.log(selPaises);
-*/
-
-window.addEventListener('load',function(){
-    selPaises = document.getElementById('selPaises');
-    selCiudades = document.getElementById('selCiudades');
-
-    cargarSelect(selPaises,obtenerPaises(datos));
-    cargarSelect(selCiudades,obtenerCiudades(datos,selPaises.value));
+window.addEventListener("load", function(){
     
-    selPaises.addEventListener('change',e=>{        
-        cargarSelect(selCiudades,obtenerCiudades(datos,selPaises.value));        
-    });
-});
+    selPaises = document.getElementById("selPaises");
+    selCiudades = document.getElementById("selCiudades");
 
-function obtenerPaises(array)
-{
-    //map recibe -> valor,indice,array
-    // let paises=array.map(function(element){
-    //     return element.pais;
-    // });
+    cargarSelect(selPaises, obtenerPaises(datos));
+    cargarSelect(selCiudades, obtenerCiudades(datos, selPaises.value));
 
-    //Version archicada
-    // let paises=array.map((element)=>{
-    //     return element.pais;
-    // });
+    selPaises.addEventListener("change", (e)=>{
+        cargarSelect(selCiudades, obtenerCiudades(datos, e.target.value));
+    })
 
-    //version achicada 2
-    // let paises=array.map(element=>{
-    //     return element.pais;
-    // });
+})
 
-    //version achicada 3 (pq tiene 1 solo return)
-    let paises=array.map(element=> element.pais)
+
+function obtenerPaises(arr){
+    
+    return arr.map(element=> element.pais)
     .unique()
     .sort();
-
-    console.log(paises);
 }
 
-function obtenerCiudades(array,pais)
-{
-    // let ciudades= array.filter(function (element){
-    //     return element.pais===pais;
-    // } );
+function obtenerCiudades(arr, pais){
 
-    return array.filter(element=>element.pais===pais)
-    .map(element=>element.ciudad)
+    return arr.filter(element => element.pais === pais)
+    .map(element => element.ciudad)
     .unique()
     .sort();
- 
 }
 
-/***
- * Parametros
- * sel = select donde cargar datos
- * array=  array de string de datos a cargar
- */
-function cargarSelect(sel,array)
-{
-    //selCiudades.opcion.length=0;
-    array.array.forEach(element => {
-        let opcion = document.createElement('option');
-        opcion.setAttribute('value',element);
-        let texto=document.createTextNode(element);
+function cargarSelect(sel, arr){
+    
+    limpiarSelect(sel);
+
+    arr.forEach(element => {
+        let opcion = document.createElement("option");
+        opcion.setAttribute("value", element);
+        let texto = document.createTextNode(element);
         opcion.appendChild(texto);
         sel.appendChild(opcion);
     });
-
 }
 
 Array.prototype.unique = function(){
     return [...new Set(this)];
 }
 
-function limpiarSelect (sel){
-    sel.opcion.length=0;
+// function limpiarSelect(sel){
+//     for(option in sel){
+//         sel.remove(option);
+//     }
+// }
 
+function limpiarSelect(sel){
     while(sel.hasChildNodes()){
-        
+        sel.remove(sel.firsElementChild);
     }
 }
---
+++ clase8/js/index.old.js
@@ -0,0 +1,89 @@
+  
+let selPaises;
+let selCiudades;
+
+/*
+lo que logra que funcione con el defer en el scr del index
+let selPaises = document.getElementById("selPaises");
+console.log(selPaises);
+*/
+
+
+
+ window.addEventListener('load',function(){
+     selPaises = document.getElementById('selPaises');
+     selCiudades = document.getElementById('selCiudades');
+
+     cargarSelect(selPaises,obtenerPaises(datos));
+     cargarSelect(selCiudades,obtenerCiudades(datos,selPaises.value));
+    
+     selPaises.addEventListener('change',e=>{        
+         cargarSelect(selCiudades,obtenerCiudades(datos,selPaises.value));        
+     });
+ });
+
+function obtenerPaises(array)
+{
+    //map recibe -> valor,indice,array
+    // let paises=array.map(function(element){
+    //     return element.pais;
+    // });
+
+    //Version archicada
+    // let paises=array.map((element)=>{
+    //     return element.pais;
+    // });
+
+    //version achicada 2
+    // let paises=array.map(element=>{
+    //     return element.pais;
+    // });
+
+    //version achicada 3 (pq tiene 1 solo return)
+    return array.map(element=> element.pais)
+    .unique()
+    .sort();
+}
+
+function obtenerCiudades(array,pais)
+{
+    // let ciudades= array.filter(function (element){
+    //     return element.pais===pais;
+    // } );
+
+    return array.filter(element=>element.pais===pais)
+    .map(element=>element.ciudad)
+    .unique()
+    .sort();
+ 
+}
+
+/***
+ * Parametros
+ * sel = select donde cargar datos
+ * array=  array de string de datos a cargar
+ */
+function cargarSelect(sel,arr)
+{
+    limpiarSelect(sel);
+
+    arr.array.forEach(element => {
+        let opcion = document.createElement('option');
+        opcion.setAttribute('value',element);
+        let texto=document.createTextNode(element);
+        opcion.appendChild(texto);
+        sel.appendChild(opcion);
+    });
+
+}
+
+Array.prototype.unique = function(){
+    return [...new Set(this)];
+}
+
+    function limpiarSelect(sel){
+        while(sel.hasChildNodes()){
+            sel.remove(sel.firsElementChild);
+    }
+
+}
