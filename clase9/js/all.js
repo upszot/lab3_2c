c6b89f151cecc87da6b3dba45299d833d7fdf82f
upszot <upszot@gmail.com>
2019-11-12 20:45:19 -0300

    clase 10

--- clase9/js/all.js
+++ clase9/js/all.js
@@ -1,33 +1,75 @@
 "use strict";
+/*
 //let mensaje:string ="hola mundo";
+
 //recibe string o number o boolean
-let mensaje = true;
+let mensaje:string |number | boolean=true;
+
+
 //recibe lo que sea
-let cualquierCosa = true;
+let cualquierCosa:any=true;
+
 console.log(mensaje);
+
 //Array
-let vector = [1, 2, 3, 4];
+let vector:number[] = [1,2,3,4];
+
 //Tupla
-let tupla = [1, "Ironman"];
+let tupla:[number,string]=[1,"Ironman"];
+
 //enum
-var Eheroes;
-(function (Eheroes) {
-    Eheroes[Eheroes["xmen"] = 0] = "xmen";
-    Eheroes[Eheroes["avenger"] = 1] = "avenger";
-})(Eheroes || (Eheroes = {}));
+enum Eheroes{
+    xmen,
+    avenger
+}
+
 console.log("enum: ");
 console.log(Eheroes.avenger);
 console.log(Eheroes[Eheroes.avenger]);
-for (let key in Eheroes) {
+for(let key in Eheroes){
     console.log(key);
 }
+*/
 //**funtionEnviarMision = devuelve el heroe enviado  */
 // con parametro opcional ?
-let funtionEnviarMision = function (heroe) {
-    return heroe + " enviado";
+//let funtionEnviarMision = function(heroe?:string):string{    
+//return heroe + " enviado";
+//}
+let funtionEnviarMision = function (...heroes) {
+    for (let i = 0; i < length; i++) {
+        console.log(heroes[i] + " enviado");
+    }
 };
-console.log(funtionEnviarMision(Eheroes[Eheroes.avenger]));
-console.log(funtionEnviarMision());
+funtionEnviarMision("Batman", "Ironman", "Hulk");
+let iroman = {
+    nombre: "Tony Stark",
+    edad: 24,
+    getNombre: function () { return this.nombre; }
+};
+console.log(iroman.getNombre());
+let wolvering = {
+    nombre: "james",
+    poder: "lala"
+};
+console.log(wolvering.nombre);
+//Interface en clases
+class Avenger {
+    constructor() {
+        this.nombre = "un avenger";
+    }
+}
+class Mutante {
+    constructor() {
+        this.nombre = "un mutante";
+    }
+}
+let unAvenger = new Avenger();
+let unMutante = new Mutante();
+console.log("unAvenger: " + unAvenger.nombre);
+console.log("unMutante: " + unMutante.nombre);
+let miFuncion;
+miFuncion = (num1, num2) => num1 + num2;
+console.log(miFuncion(1, 5));
 /// <reference path="hello.ts" />
 let mensaje2 = "chau";
 console.log(mensaje2);
--- clase9/ts/hello.ts
+++ clase9/ts/hello.ts
@@ -1,3 +1,4 @@
+/*
 //let mensaje:string ="hola mundo";
 
 //recibe string o number o boolean
@@ -27,14 +28,84 @@ console.log(Eheroes[Eheroes.avenger]);
 for(let key in Eheroes){
     console.log(key);
 }
+*/
 
 //**funtionEnviarMision = devuelve el heroe enviado  */
 // con parametro opcional ?
-let funtionEnviarMision = function(heroe?:string):string{    
-    return heroe + " enviado";
+//let funtionEnviarMision = function(heroe?:string):string{    
+    //return heroe + " enviado";
+//}
+
+let funtionEnviarMision = function(...heroes:string[]):void{    
+    for(let i=0;i<length;i++)
+    {
+        console.log(heroes[i] + " enviado");
+    }    
 }
 
-console.log(funtionEnviarMision (Eheroes[Eheroes.avenger]));
 
-console.log(funtionEnviarMision ());
+funtionEnviarMision("Batman","Ironman","Hulk");
 
+//console.log(funtionEnviarMision (Eheroes[Eheroes.avenger]));
+
+//console.log(funtionEnviarMision ());
+
+//tipo en el objeto
+/*
+let flash:{nombre:string,edad:number,poderes:string[],getNombre:()=>string}=
+{
+    nombre: "Barry allen",
+    edad:24,
+    poderes:string[],
+    getNombre(){
+        return this.nombre;
+    }
+};
+*/
+
+
+//tipo personalizado
+type Heroe={nombre:string,edad:number,poderes?:string[],getNombre:()=>string};
+let iroman:Heroe={
+    nombre:"Tony Stark",
+    edad:24,
+    getNombre:function(){return this.nombre;}
+}
+console.log(iroman.getNombre());
+
+//interfaces
+interface IHeroe{
+    nombre:string,
+    poder?:string,
+    mostrar?():string
+}
+let wolvering:IHeroe={
+    nombre: "james",
+    poder:"lala"
+};
+
+console.log(wolvering.nombre);
+
+//Interface en clases
+class Avenger implements IHeroe{
+    nombre:string="un avenger";
+}
+
+class Mutante implements IHeroe{
+    nombre:string="un mutante";
+}
+
+let unAvenger=new Avenger();
+let unMutante=new Mutante();
+
+console.log("unAvenger: " + unAvenger.nombre);
+console.log("unMutante: " + unMutante.nombre);
+
+
+interface iFuncDosNumeros{
+    (num1:number,num2:number):number;
+}
+let miFuncion:iFuncDosNumeros;
+miFuncion=(num1:number,num2:number)=>num1+num2;
+
+console.log(miFuncion(1,5));
